7e0a5f2c779d7fd99f9bfe153dc4d29085f3450e
IzumiSy <beetle-noise@gmx.com>
2024-09-25 17:34:59 +0900

    Update structure

---
+++ examples/todoapp/src/App.tsx
@@ -0,0 +1,51 @@
+import { Heading, Stack } from "@chakra-ui/react";
+import { FabrixComponent, gql } from "@fabrix-framework/fabrix";
+import { css } from "@emotion/css";
+
+const containerClassName = css`
+  padding: 15px 0;
+`;
+
+function App() {
+  return (
+    <Stack padding={10}>
+      <Heading textAlign="center" size="md">
+        Fabrix TODO app example
+      </Heading>
+      <FabrixComponent
+        containerClassName={containerClassName}
+        query={gql`
+          mutation createTodo($input: TodoInput!) {
+            addTodo(input: $input)
+              @fabrixForm(
+                input: [
+                  { field: "hasDone", config: { hidden: true } }
+                  { field: "name", config: { gridCol: 9 } }
+                  { field: "priority", config: { gridCol: 3 } }
+                ]
+              ) {
+              id
+            }
+          }
+
+          query todos {
+            allTodos
+              @fabrixView(
+                input: [
+                  { field: "collection", config: { label: "Your tasks" } }
+                ]
+              ) {
+              collection {
+                name
+                priority
+                hasDone
+              }
+            }
+          }
+        `}
+      />
+    </Stack>
+  );
+}
+
+export default App;
