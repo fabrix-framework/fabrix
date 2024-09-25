b0b1486226dd76c96aa3b2b1f877f7f22b47c957
IzumiSy <beetle-noise@gmx.com>
2024-09-25 18:23:33 +0900

    Install tsx as a replacement of ts-node, modified eslint configuration

--- examples/todoapp/eslint.config.js
+++ examples/todoapp/eslint.config.js
@@ -1,28 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js";
+import react from "eslint-plugin-react";
+import tsEslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+export default tsEslint.config({
+  files: ["**/*.{ts,tsx}"],
+  extends: [eslint.configs.recommended, ...tsEslint.configs.recommended],
+  languageOptions: {
+    parser: tsEslint.parser,
   },
-)
+  plugins: {
+    react,
+  },
+});
