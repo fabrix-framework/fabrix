5f640840db0fc9f3fcaa38a350cca5d55c9ed827
IzumiSy <beetle-noise@gmx.com>
2024-10-16 16:41:49 +0900

    Split packages by node.js dependencies

--- packages/graphql-config/src/config.ts
+++ packages/graphql-config/src/config.ts
@@ -1,10 +1,8 @@
 import * as os from "node:os";
 import * as path from "node:path";
 import * as fs from "node:fs";
-import { parse } from "graphql";
 import Document from "./directive.graphql";
 
-export const schemaDefinition = parse(Document);
 export const generateConfig = () => {
   const tempGQLFile = path.join(os.tmpdir(), "fabrix-graphql-config.graphql");
 
