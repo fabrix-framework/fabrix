bd8ce818f67688d2c4e86984467a9438bb699e2a
IzumiSy <beetle-noise@gmx.com>
2024-10-18 13:13:19 +0900

    Split GraphQL definition files

--- packages/graphql-config/src/config.ts
+++ packages/graphql-config/src/config.ts
@@ -1,12 +1,18 @@
 import * as os from "node:os";
 import * as path from "node:path";
 import * as fs from "node:fs";
-import Document from "./directive.graphql";
+import CommonSchema from "./schema/common.graphql";
+import ViewDirectiveSchema from "./schema/view.graphql";
+import FormDirectiveSchema from "./schema/form.graphql";
+import ConstraintSchema from "./schema/constraint.graphql";
 
 export const generateConfig = () => {
   const tempGQLFile = path.join(os.tmpdir(), "fabrix-graphql-config.graphql");
 
-  fs.writeFileSync(tempGQLFile, Document);
+  fs.writeFileSync(
+    tempGQLFile,
+    CommonSchema + ViewDirectiveSchema + FormDirectiveSchema + ConstraintSchema,
+  );
 
   return {
     directiveSchema: tempGQLFile,
