13f248f5014042245ad9a6a1732aa8e6ce938930
IzumiSy <beetle-noise@gmx.com>
2024-10-28 19:02:42 +0900

    Use buildASTSchema to test schema

--- packages/graphql-config/src/schema.test.ts
+++ packages/graphql-config/src/schema.test.ts
@@ -1,11 +1,9 @@
 import { describe, it, expect } from "vitest";
+import { buildASTSchema } from "graphql";
 import { schemaDefinition } from "./schema";
 
-describe("schemaDefinitions", () => {
-  it("should have valid definitions", () => {
-    expect(schemaDefinition.definitions.length).not.toBeLessThanOrEqual(0);
-    schemaDefinition.definitions.forEach((def) => {
-      expect(def).not.toBeUndefined();
-    });
+describe("schemaDefinition", () => {
+  it("should be buildable", () => {
+    expect(buildASTSchema(schemaDefinition)).not.toThrow();
   });
 });
