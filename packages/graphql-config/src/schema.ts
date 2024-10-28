90af5725f50abb4e0e612afbe677f1514b35ad47
IzumiSy <beetle-noise@gmx.com>
2024-10-28 18:45:59 +0900

    Use @graphql-tools/merge

--- packages/graphql-config/src/schema.ts
+++ packages/graphql-config/src/schema.ts
@@ -1,17 +1,18 @@
-import { Kind, parse } from "graphql";
+import { parse } from "graphql";
+import { mergeTypeDefs } from "@graphql-tools/merge";
 import CommonSchema from "./schema/common.graphql";
 import ViewDirectiveSchema from "./schema/view.graphql";
 import FormDirectiveSchema from "./schema/form.graphql";
 import ConstraintSchema from "./schema/constraint.graphql";
 
-const mergeDocumentNodes = (rawDefinition: string[]) => ({
-  kind: Kind.DOCUMENT as const,
-  definitions: rawDefinition.map((def) => parse(def)),
-});
+const parseStringSchemas = (rawDefinition: string[]) =>
+  rawDefinition.map((def) => parse(def));
 
-export const schemaDefinition = mergeDocumentNodes([
-  CommonSchema,
-  ViewDirectiveSchema,
-  FormDirectiveSchema,
-  ConstraintSchema,
-]);
+export const schemaDefinition = mergeTypeDefs(
+  parseStringSchemas([
+    CommonSchema,
+    ViewDirectiveSchema,
+    FormDirectiveSchema,
+    ConstraintSchema,
+  ]),
+);
