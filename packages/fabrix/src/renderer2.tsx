749ae00d17ed0e208abf69cbfb167d1940fc004b
IzumiSy <beetle-noise@gmx.com>
2024-12-02 14:37:12 +0900

    WIP

--- packages/fabrix/src/renderer2.tsx
+++ packages/fabrix/src/renderer2.tsx
@@ -3,7 +3,7 @@ import { ComponentEntry } from "@registry2";
 import { FabrixComponentProps, useFieldConfigs } from "@renderer";
 import { DocumentResolver, Loader } from "@renderers/shared";
 
-const FabrixComponent2 = (
+export const FabrixComponent2 = (
   props: FabrixComponentProps & {
     componentType: Exclude<
       ComponentEntry["type"],
@@ -11,9 +11,8 @@ const FabrixComponent2 = (
     >;
   },
 ) => {
-  const fieldConfigs = useFieldConfigs(props.query);
-
   switch (props.componentType) {
+    // NOTE: Only table is implemented here as WIP
     case "table": {
       return (
         <DataFetcher
@@ -21,28 +20,39 @@ const FabrixComponent2 = (
           variables={props.variables}
           defaultData={props.data}
         >
-          {({ data }) => {
-            return (
-              <>
-                <h1>query</h1>
-                <div>{props.componentType}</div>
-              </>
-            );
-          }}
+          {({ data }) => <TableRenderer {...props} data={data} />}
         </DataFetcher>
-      );
-    }
-    case "form": {
-      return (
-        <>
-          <h1>form</h1>
-          <div>{props.componentType}</div>
-        </>
       );
     }
     default:
       return null;
   }
+};
+
+const TableRenderer = (props: FabrixComponentProps) => {
+  const fieldConfigs = useFieldConfigs(props.query);
+
+  if (fieldConfigs.length > 1 || fieldConfigs.length === 0) {
+    throw new Error("Table requires only one field at the root level");
+  }
+  const fielConfig = fieldConfigs[0];
+  const fieldKeys = Object.keys(fielConfig);
+
+  const tableComponents = fieldKeys.map((key) => {
+    const field = fielConfig[key];
+    if (field.type !== "view") {
+      throw new Error("Table requires only view fields");
+    }
+
+    return <div>TableRender</div>;
+  });
+
+  return (
+    <>
+      <h1>query</h1>
+      <div>{tableComponents}</div>
+    </>
+  );
 };
 
 const DataFetcher = (props: {
