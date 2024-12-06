cacc47b95354642620e207d4e64bf292bd2ef55c
IzumiSy <beetle-noise@gmx.com>
2024-12-06 17:44:00 +0900

    Type-safe merging in ComponentRegistry

--- packages/fabrix/src/index.ts
+++ packages/fabrix/src/index.ts
@@ -6,11 +6,16 @@ export { useFabrixClient, useDataFetch } from "@fetcher";
 export { type FieldType } from "@renderers/shared";
 export { type SubField } from "@renderers/fields";
 export {
+  type FieldComponentEntry,
   type FieldComponentProps,
+  type TableComponentEntry,
   type TableComponentProps,
   type TableComponentHeader,
+  type TableCellComponentEntry,
   type TableCellComponentProps,
+  type FormComponentEntry,
   type FormComponentProps,
+  type FormFieldComponentEntry,
   type FormFieldComponentProps,
   ComponentRegistry,
 } from "./registry";
