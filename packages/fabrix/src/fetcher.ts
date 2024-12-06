aa6b1e418cf6a9bd7df4002092cec7c73e7d747e
Akira HIGUCHI <akira_higuchi@tailor.tech>
2024-12-06 09:27:31 +0900

    remove defaultDat

--- packages/fabrix/src/fetcher.ts
+++ packages/fabrix/src/fetcher.ts
@@ -1,28 +1,19 @@
 import { DocumentNode } from "graphql";
-import { useMemo } from "react";
 import { useClient, useQuery } from "urql";
 
 export const useDataFetch = (props: {
   query: DocumentNode | string;
   variables?: Record<string, unknown>;
-  defaultData?: FabrixComponentData;
 }) => {
-  // Stop the query from executing automatically by enabling the `pause` option.
-  // when the `defaultData` prop is provided.
-  const [{ data: queryData, fetching, error }] = useQuery<FabrixComponentData>({
+  const [{ data, fetching, error }] = useQuery<FabrixComponentData>({
     query: props.query,
-    pause: props.defaultData !== undefined,
     variables: props.variables,
   });
-
-  const renderingData = useMemo(() => {
-    return props.defaultData === undefined ? queryData : props.defaultData;
-  }, [props.defaultData, queryData]);
 
   return {
     fetching,
     error,
-    data: renderingData,
+    data,
   };
 };
 
