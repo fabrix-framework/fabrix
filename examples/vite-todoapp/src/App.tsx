c7a985c94681a5ac469fd168842076fb0a9d0c2a
IzumiSy <beetle-noise@gmx.com>
2025-01-27 17:13:52 +0900

    Split components

--- examples/vite-todoapp/src/App.tsx
+++ examples/vite-todoapp/src/App.tsx
@@ -1,60 +1,63 @@
 import { Heading, Stack, Grid, GridItem } from "@chakra-ui/react";
 import { FabrixComponent, gql } from "@fabrix-framework/fabrix";
 
-const createTODOQuery = gql`
-  mutation createTodo($input: TodoInput!) {
-    addTodo(input: $input) {
-      id
+const NewTodoForm = () => (
+  <FabrixComponent
+    query={gql`
+      mutation createTodo($input: TodoInput!) {
+        addTodo(input: $input) {
+          id
+        }
+      }
+    `}
+  >
+    {({ getInput }) =>
+      getInput({}, ({ Action, Field }) => (
+        <Stack gridColumn={"1/12"}>
+          <Grid templateColumns={"repeat(6, 1fr)"} gap={3} autoFlow={"column"}>
+            <GridItem colSpan={4}>
+              <Field name="input.name" extraProps={{ label: "タスク名" }} />
+            </GridItem>
+            <GridItem colSpan={2}>
+              <Field name="input.priority" extraProps={{ label: "優先度" }} />
+            </GridItem>
+          </Grid>
+          <Action />
+        </Stack>
+      ))
     }
-  }
-`;
+  </FabrixComponent>
+);
 
-function App() {
+const TodoList = () => (
+  <FabrixComponent
+    query={gql`
+      query todos {
+        allTodos {
+          collection {
+            id
+            name
+            priority
+            hasDone
+          }
+        }
+      }
+    `}
+  >
+    {({ getOutput }) => getOutput("allTodos")}
+  </FabrixComponent>
+);
+
+const App = () => {
   return (
     <Stack padding={10}>
       <Heading textAlign="center" size="md">
         Fabrix TODO app example
       </Heading>
-      <FabrixComponent query={createTODOQuery}>
-        {({ getInput }) =>
-          getInput({}, ({ Action, Field }) => (
-            <Stack gridColumn={"1/12"}>
-              <Grid
-                templateColumns={"repeat(6, 1fr)"}
-                gap={3}
-                autoFlow={"column"}
-              >
-                <GridItem colSpan={4}>
-                  <Field name="input.name" extraProps={{ label: "タスク名" }} />
-                </GridItem>
-                <GridItem colSpan={2}>
-                  <Field
-                    name="input.priority"
-                    extraProps={{ label: "優先度" }}
-                  />
-                </GridItem>
-              </Grid>
-              <Action />
-            </Stack>
-          ))
-        }
-      </FabrixComponent>
-      <FabrixComponent
-        query={gql`
-          query todos {
-            allTodos @fabrixView {
-              collection {
-                id
-                name
-                priority
-                hasDone
-              }
-            }
-          }
-        `}
-      />
+      <NewTodoForm />
+      <TodoList />
     </Stack>
   );
-}
+};
 
 export default App;
